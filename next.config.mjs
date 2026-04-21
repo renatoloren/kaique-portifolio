94da4cad3259ab4089dfae0a7695669d4be85126
Renato Lourenço <renatoloren226@gmail.com>
2026-04-20 21:34:15 -0300

    adjustment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/nome-do-seu-repositorio", // Substitua pelo nome do seu repo
+  basePath: "/kaique-portifolio", // Substitua pelo nome do seu repo
   images: {
     unoptimized: true, // Necessário para GH Pages
   },
