d42cb5ffad30f4186e956945cafec94143f5dae7
Renato Lourenço <renatoloren226@gmail.com>
2026-04-20 21:32:03 -0300

    github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  output: "export",
+  basePath: "/nome-do-seu-repositorio", // Substitua pelo nome do seu repo
+  images: {
+    unoptimized: true, // Necessário para GH Pages
+  },
+};
+module.exports = nextConfig;
