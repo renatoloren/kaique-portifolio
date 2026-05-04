176af949d6f6447d027027c891135b935813c154
Renato Lourenço <renatoloren226@gmail.com>
2026-05-04 01:21:58 -0300

    welcome subtitle grid

--- src/app/components/Welcome/Welcome.jsx
+++ src/app/components/Welcome/Welcome.jsx
@@ -4,12 +4,11 @@ import styles from './Welcome.module.css'
 
 export default function Welcome() {
   return (
-      <section className={styles.welcome}>
-
+      <section className={styles.welcome_section}>
         <h1 className={styles.welcome_name}>
           kaique morais
         </h1>
-        <h1 className={styles.welcome_role}>
+        <h1 className={styles.welcome_subtitle}>
             {"Traduzindo ideias como "} 
             <span class={styles.highlight}>{"videomaker "}</span> 
             {"em linguagem visual"}
